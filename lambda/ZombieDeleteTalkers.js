97a21e9220370e05983003b9038215a71a50856a
Sankalp Acharya <sacharya@rmn.com>
2015-10-07 17:08:42 -0700

    new lambdas

---
+++ lambda/ZombieDeleteTalkers.js
@@ -0,0 +1,45 @@
+console.log('Loading function');
+var aws = require('aws-sdk');
+var ddb = new aws.DynamoDB(
+    {region: "us-west-2",
+     params: {TableName: "talkers"}});
+
+var theContext;
+
+function dynamoCallback(err, response) {
+  if (err) console.log('error' + err, err.stack); // an error occurred
+  else
+  {
+      theContext.succeed(response);
+  }
+}
+
+exports.handler = function(event, context)
+{
+    theContext = context;
+
+    if(event.message == null ||
+       event.name == null)
+    {
+        context.fail("message and name cannot be null");
+        return;
+    }
+
+    if(event.user == null)
+    {
+        event.user = 'default';
+    }
+    if(event.timestamp == null)
+    {
+        event.timestamp = "" + new Date().getTime();
+    }
+  var params =
+      {
+            "TableName": 'talkers',
+            "Key": {
+                "name":{"S":event.user}
+            }
+        }
+    console.log("Putting item DynamoDB " + params);
+     ddb.deleteItem(params, dynamoCallback);
+}
---
+++ lambda/ZombiePostTalkers.js
@@ -0,0 +1,46 @@
+console.log('Loading function');
+var aws = require('aws-sdk');
+var ddb = new aws.DynamoDB(
+    {region: "us-west-2",
+     params: {TableName: "talkers"}});
+
+var theContext;
+
+function dynamoCallback(err, response) {
+  if (err) console.log('error' + err, err.stack); // an error occurred
+  else
+  {
+      theContext.succeed(response);
+  }
+}
+
+exports.handler = function(event, context)
+{
+    theContext = context;
+
+    if(event.message == null ||
+       event.name == null)
+    {
+        context.fail("message and name cannot be null");
+        return;
+    }
+
+    if(event.user == null)
+    {
+        event.user = 'default';
+    }
+    if(event.timestamp == null)
+    {
+        event.timestamp = "" + new Date().getTime();
+    }
+  var params =
+      {
+            "Item":
+            {
+                "name":{"S":event.user},
+                "is_typing":{"B": true}
+            }
+        }
+    console.log("Putting item DynamoDB " + params);
+     ddb.putItem(params, dynamoCallback);
+}
